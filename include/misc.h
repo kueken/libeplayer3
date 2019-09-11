1682d88b9ded8671f1ba05651d9408ba88bb2d33
kueken <ohnemailadresse@arcor.de>
2019-09-11 22:33:33 +0200

    Revert "try update misc handler from exteplayer3 thx"
    This reverts commit d7e42e5aa3dd3b2e411eed0330cb705f64b31541.

--- include/misc.h
+++ include/misc.h
@@ -3,23 +3,18 @@
 
 /* some useful things needed by many files ... */
 
-#include <dirent.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
 #include <stdint.h>
 
 #define INVALID_PTS_VALUE	0x200000000ll
 
 struct BitPacker_t
 {
-	uint8_t   *Ptr;         /* write pointer */
-	uint32_t   BitBuffer;   /* bitreader shifter */
-	int32_t    Remaining;   /* number of remaining in the shifter */
+	uint8_t *Ptr;		/* write pointer */
+	unsigned int BitBuffer;	/* bitreader shifter */
+	int Remaining;		/* number of remaining in the shifter */
 };
 
-void PutBits(BitPacker_t *ld, uint32_t code, uint32_t length);
-void FlushBits(BitPacker_t *ld);
-int8_t PlaybackDieNow(int8_t val);
+void PutBits(BitPacker_t * ld, unsigned int code, unsigned int length);
+void FlushBits(BitPacker_t * ld);
 
 #endif
